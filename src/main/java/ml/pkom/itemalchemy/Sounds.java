cab2fcfc80130b02c4bdf05e227ba638de18d424
PTOM76 <ptms76@outlook.jp>
2022-12-09 17:26:24 +0900

    1.19.3

--- src/main/java/ml/pkom/itemalchemy/Sounds.java
+++ src/main/java/ml/pkom/itemalchemy/Sounds.java
@@ -1,15 +1,17 @@
 package ml.pkom.itemalchemy;
 
+import ml.pkom.mcpitanlibarch.api.event.registry.RegistryEvent;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class Sounds {
-    public static final Identifier EXCHANGE_SOUND = ItemAlchemy.id("exchange");
-    public static SoundEvent EXCHANGE_SOUND_EVENT = new SoundEvent(EXCHANGE_SOUND);
-
+    public static final RegistryEvent<SoundEvent> EXCHANGE_SOUND = register("exchange");
 
     public static void init(){
-        Registry.register(Registry.SOUND_EVENT, EXCHANGE_SOUND, EXCHANGE_SOUND_EVENT);
+
+    }
+    private static RegistryEvent<SoundEvent> register(String id) {
+        return ItemAlchemy.registry.registerSoundEvent(ItemAlchemy.id(id));
     }
 }
