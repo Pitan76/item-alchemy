91220523b1a2a3f160a8d056578760b98fa43fde
PTOM76 <ptms76@outlook.jp>
2022-12-13 11:39:49 +0900

    add copy from #7

---
+++ src/main/java/ml/pkom/itemalchemy/mixins/ServerPlayerEntityMixin.java
@@ -0,0 +1,28 @@
+package ml.pkom.itemalchemy.mixins;
+
+import ml.pkom.mcpitanlibarch.api.nbt.NbtTag;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.server.network.ServerPlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ServerPlayerEntity.class)
+public abstract class ServerPlayerEntityMixin {
+    @Shadow public abstract void writeCustomDataToNbt(NbtCompound nbt);
+
+    @Shadow public abstract void readCustomDataFromNbt(NbtCompound nbt);
+
+    @Inject(method = "copyFrom", at = @At("TAIL"))
+    private void copyFrom(ServerPlayerEntity oldPlayer, boolean alive, CallbackInfo ci) {
+        NbtTag oldNbt = new NbtTag();
+        NbtTag newNbt = new NbtTag();
+        oldPlayer.writeCustomDataToNbt(oldNbt);
+        NbtCompound itemAlchemy = oldNbt.getCompound("itemalchemy");
+        this.writeCustomDataToNbt(newNbt);
+        newNbt.put("itemalchemy", itemAlchemy);
+        this.readCustomDataFromNbt(newNbt);
+    }
+}
