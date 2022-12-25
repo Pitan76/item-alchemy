9a251b89baef0052925c1fc24749b6453e2f6282
PTOM76 <ptms76@outlook.jp>
2022-12-25 23:40:15 +0900

    fix

--- src/main/java/ml/pkom/itemalchemy/mixins/PlayerEntityMixin.java
+++ src/main/java/ml/pkom/itemalchemy/mixins/PlayerEntityMixin.java
@@ -18,24 +18,25 @@ public class PlayerEntityMixin {
     @Inject(method = "writeCustomDataToNbt", at = @At("TAIL"))
     public void writeCustomDataToNbt(NbtCompound nbt, CallbackInfo ci) {
         Player player = new Player((PlayerEntity) (Object) this);
-        NbtCompound cachedPlayerNbt = EMCManager.writePlayerNbt(player);
-        if (cachedPlayerNbt.contains("itemalchemy"))
-            itemAlchemy = cachedPlayerNbt.getCompound("itemalchemy");
 
-        nbt.put("itemalchemy", itemAlchemy);
+        NbtCompound cachedPlayerNbt = EMCManager.writePlayerNbt(player);
+        if (cachedPlayerNbt.contains("itemalchemy")) {
+            itemAlchemy = cachedPlayerNbt.getCompound("itemalchemy");
+            nbt.put("itemalchemy", itemAlchemy);
+        }
+
     }
 
     @Inject(method = "readCustomDataFromNbt", at = @At("TAIL"))
     public void readCustomDataFromNbt(NbtCompound nbt, CallbackInfo ci) {
         Player player = new Player((PlayerEntity) (Object) this);
 
+        itemAlchemy = nbt.getCompound("itemalchemy");
         if (EMCManager.playerCache.containsKey(player.getName())) {
             EMCManager.playerCache.replace(player.getName(), nbt);
         } else {
             EMCManager.playerCache.put(player.getName(), nbt);
         }
-
-        itemAlchemy = nbt.getCompound("itemalchemy");
         if ((Object) this instanceof ServerPlayerEntity) {
             EMCManager.syncS2C((ServerPlayerEntity) (Object) this);
         }
