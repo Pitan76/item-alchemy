7e0f1d238ef768f7db300571dfcdfa318c2bb4c5
PTOM76 <ptms76@outlook.jp>
2023-06-23 18:11:57 +0900

    Add charge/uncharge sounds from PE

--- src/main/java/ml/pkom/itemalchemy/sound/Sounds.java
+++ src/main/java/ml/pkom/itemalchemy/sound/Sounds.java
@@ -6,6 +6,8 @@ import net.minecraft.sound.SoundEvent;
 
 public class Sounds {
     public static final RegistryEvent<SoundEvent> EXCHANGE_SOUND = register("exchange");
+    public static final RegistryEvent<SoundEvent> CHARGE_SOUND = register("charge");
+    public static final RegistryEvent<SoundEvent> UNCHARGE_SOUND = register("uncharge");
 
     public static void init(){
 
