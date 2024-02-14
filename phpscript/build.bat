7afb811f44034c4332ef630b66707d3cc4ab60fe
PTOM76 <58260965+PTOM76@users.noreply.github.com>
2024-02-15 01:46:25 +0900

    bat

--- phpscript/build.bat
+++ phpscript/build.bat
@@ -8,6 +8,8 @@ call gradlew.bat build
 call gradlew.bat generatePomFileForMavenJavaPublication
 copy /y ".\build\publications\mavenJava\pom-default.xml" ".\build\publications\mavenJava\%modid%.pom"
 
+timeout /T 1
+
 cd ".\php"
 
 start upload_maven.bat
