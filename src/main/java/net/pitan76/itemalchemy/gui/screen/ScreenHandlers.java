8dd1eaea70308b757e0c91e262dd1490bf2f3fa5
PTOM76 <58260965+PTOM76@users.noreply.github.com>
2024-02-23 12:19:27 +0900

    use new mcpitanlib

--- src/main/java/net/pitan76/itemalchemy/gui/screen/ScreenHandlers.java
+++ src/main/java/net/pitan76/itemalchemy/gui/screen/ScreenHandlers.java
@@ -5,6 +5,8 @@ import net.pitan76.mcpitanlib.api.gui.ExtendedScreenHandlerTypeBuilder;
 import net.pitan76.mcpitanlib.api.gui.SimpleScreenHandlerTypeBuilder;
 import net.minecraft.screen.ScreenHandlerType;
 
+import static net.pitan76.itemalchemy.ItemAlchemy.registry;
+
 public class ScreenHandlers {
     public static ScreenHandlerType<AlchemyTableScreenHandler> ALCHEMY_TABLE = new SimpleScreenHandlerTypeBuilder<>(AlchemyTableScreenHandler::new).build();
     public static ScreenHandlerType<EMCCollectorScreenHandler> EMC_COLLECTOR = new ExtendedScreenHandlerTypeBuilder<>(EMCCollectorScreenHandler::new).build();
@@ -13,9 +15,9 @@ public class ScreenHandlers {
     public static ScreenHandlerType<AlchemyChestScreenHandler> ALCHEMY_CHEST = new SimpleScreenHandlerTypeBuilder<>(AlchemyChestScreenHandler::new).build();
 
     public static void init() {
-        ItemAlchemy.registry.registerScreenHandlerType(ItemAlchemy.id("alchemy_table"), () -> ALCHEMY_TABLE);
-        ItemAlchemy.registry.registerScreenHandlerType(ItemAlchemy.id("emc_collector"), () -> EMC_COLLECTOR);
-        ItemAlchemy.registry.registerScreenHandlerType(ItemAlchemy.id("emc_condenser"), () -> EMC_CONDENSER);
-        ItemAlchemy.registry.registerScreenHandlerType(ItemAlchemy.id("alchemy_chest"), () -> ALCHEMY_CHEST);
+        registry.registerScreenHandlerType(ItemAlchemy.id("alchemy_table"), () -> ALCHEMY_TABLE);
+        registry.registerScreenHandlerType(ItemAlchemy.id("emc_collector"), () -> EMC_COLLECTOR);
+        registry.registerScreenHandlerType(ItemAlchemy.id("emc_condenser"), () -> EMC_CONDENSER);
+        registry.registerScreenHandlerType(ItemAlchemy.id("alchemy_chest"), () -> ALCHEMY_CHEST);
     }
 }
