0f4eff2e9dacdcc1bfd0131b556775e39b885cc3
Anthony Mendez <anthonymendez9@gmail.com>
2024-08-05 01:33:37 -0400

    Refactor ItemMixin.java charge behavior to ItemUtils.java

--- src/main/java/net/pitan76/itemalchemy/util/ItemUtils.java
+++ src/main/java/net/pitan76/itemalchemy/util/ItemUtils.java
@@ -2,12 +2,17 @@ package net.pitan76.itemalchemy.util;
 
 import static com.google.common.primitives.Ints.constrainToRange;
 
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.world.World;
 import net.pitan76.itemalchemy.ItemAlchemy;
+import net.pitan76.itemalchemy.mixins.ItemMixin;
 import net.pitan76.mcpitanlib.api.util.CustomDataUtil;
+import org.apache.logging.log4j.Level;
 import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 /**
  * Provides utility functions relating to {@link ItemStack}s and their {@link ItemAlchemy} charge
@@ -22,6 +27,18 @@ public class ItemUtils {
   public static final int MIN_CHARGE_VALUE = 0;
   // Maximum charge value allowed.
   public static final int MAX_CHARGE_VALUE = 4;
+
+  /**
+   * Handles {@link ItemMixin#inventoryTick(ItemStack, World, Entity, int, boolean, CallbackInfo)}
+   * if the selected inventory item is of {@link ItemCharge}.
+   *
+   * @param itemStack that is selected and of {@link ItemCharge}.
+   */
+  public static void handleItemChargeInventoryTick(ItemStack itemStack) {
+    int charge = getCharge(itemStack);
+    int damage = itemStack.getMaxDamage() - (charge * 4);
+    itemStack.setDamage(damage);
+  }
 
   /**
    * Returns the current {@link ItemStack} in the {@link PlayerEntity}'s hand, or offhand if the
@@ -46,14 +63,29 @@ public class ItemUtils {
   }
 
   /**
-   * Returns the charge value of an {@link ItemStack}.
+   * Checks if the given {@link ItemStack} is chargeable via {@link ItemCharge}.
+   *
+   * @param stack to check.
+   * @return false if {@code stack} is null or not chargeable. True if stack is non-null and
+   *     chargeable.
+   */
+  public static boolean isItemChargeable(@Nullable ItemStack stack) {
+    if (stack == null) {
+      return false;
+    }
+
+    return stack.getItem() instanceof ItemCharge;
+  }
+
+  /**
+   * Returns the charge value of an {@link ItemStack}. If the {@link ItemStack} does not have a
+   * charge value set, it sets it to 0 and returns 0.
    *
    * @param stack of the item to get the charge value.
    * @return {@code int} of the charge value between [0-4].
    */
   public static int getCharge(ItemStack stack) {
-    boolean isChargeableItem = stack.getItem() instanceof ItemCharge;
-    if (!isChargeableItem) {
+    if (!isItemChargeable(stack)) {
       return MIN_CHARGE_VALUE;
     }
 
@@ -61,7 +93,9 @@ public class ItemUtils {
 
     if (!component.contains(CHARGE_COMPONENT_KEY)) {
       setCharge(stack, MIN_CHARGE_VALUE);
+      return MIN_CHARGE_VALUE;
     }
+
     return component.getInt(CHARGE_COMPONENT_KEY);
   }
 
@@ -72,11 +106,11 @@ public class ItemUtils {
    * @param charge value to set the {@code stack} to.
    */
   public static void setCharge(ItemStack stack, int charge) {
-    boolean isChargeableItem = stack.getItem() instanceof ItemCharge;
-    if (!isChargeableItem) {
+    if (!isItemChargeable(stack)) {
       return;
     }
 
+    // Needed as method is under Guava beta right now.
     //noinspection UnstableApiUsage
     charge = constrainToRange(charge, MIN_CHARGE_VALUE, MAX_CHARGE_VALUE);
 
