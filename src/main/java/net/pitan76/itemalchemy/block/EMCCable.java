d0cfd25945e83964bdd93dce467156d43650b62b
PTOM76 <58260965+PTOM76@users.noreply.github.com>
2024-09-19 02:57:42 +0900

    fix emc_cable

--- src/main/java/net/pitan76/itemalchemy/block/EMCCable.java
+++ src/main/java/net/pitan76/itemalchemy/block/EMCCable.java
@@ -27,7 +27,9 @@ public class EMCCable extends EMCRepeater implements IUseableWrench {
     public static final DirectionProperty FACING = PropertyUtil.createDirectionProperty("facing");
 
     public static final VoxelShape NONE = VoxelShapeUtil.blockCuboid(6.0D, 6.0D, 6.0D, 10.0D, 10.0D, 10.0D);
-    public static final VoxelShape BOTH_CONNECT = VoxelShapeUtil.blockCuboid(0.0D, 6.0D, 6.0D, 16.0D, 10.0D, 10.0D);
+    public static final VoxelShape NS_BOTH_CONNECT = VoxelShapeUtil.blockCuboid(0.0D, 6.0D, 6.0D, 16.0D, 10.0D, 10.0D);
+    public static final VoxelShape EW_BOTH_CONNECT = VoxelShapeUtil.blockCuboid(6.0D, 6.0D, 0.0D, 10.0D, 10.0D, 16.0D);
+
     public static final VoxelShape ONE_CONNECT = VoxelShapeUtil.blockCuboid(0.0D, 6.0D, 6.0D, 13.0D, 10.0D, 10.0D);
     public static final VoxelShape CONNER_CONNECT = VoxelShapeUtil.union(VoxelShapeUtil.blockCuboid(0.0D, 6.0D, 6.0D, 10.0D, 10.0D, 10.0D), VoxelShapeUtil.blockCuboid(6.0D, 6.0D, 0.0D, 10.0D, 10.0D, 10.0D));
 
@@ -103,7 +105,11 @@ public class EMCCable extends EMCRepeater implements IUseableWrench {
     @Override
     public VoxelShape getOutlineShape(OutlineShapeEvent e) {
         if (e.getState().get(SIDE1) && e.getState().get(SIDE2)) {
-            return BOTH_CONNECT;
+            if (e.getState().get(FACING) == Direction.NORTH || e.getState().get(FACING) == Direction.SOUTH) {
+                return NS_BOTH_CONNECT;
+            } else {
+                return EW_BOTH_CONNECT;
+            }
         } else if (e.getState().get(SIDE1) || e.getState().get(SIDE2)) {
             return ONE_CONNECT;
         } else if (e.getState().get(CONNER)) {
