44560ee6156a8b19fca37626e5953d8f70c1333b
PTOM76 <58260965+PTOM76@users.noreply.github.com>
2024-09-19 15:41:21 +0900

    use mcpitanlib

--- src/main/java/net/pitan76/itemalchemy/block/EMCCable.java
+++ src/main/java/net/pitan76/itemalchemy/block/EMCCable.java
@@ -2,24 +2,24 @@ package net.pitan76.itemalchemy.block;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.Waterloggable;
+import net.minecraft.fluid.FluidState;
 import net.minecraft.state.property.BooleanProperty;
 import net.minecraft.state.property.DirectionProperty;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.shape.VoxelShape;
-import net.minecraft.world.WorldAccess;
 import net.pitan76.itemalchemy.tile.base.EMCStorageBlockEntity;
 import net.pitan76.mcpitanlib.api.block.CompatibleBlockSettings;
-import net.pitan76.mcpitanlib.api.event.block.AppendPropertiesArgs;
-import net.pitan76.mcpitanlib.api.event.block.OutlineShapeEvent;
-import net.pitan76.mcpitanlib.api.util.PropertyUtil;
-import net.pitan76.mcpitanlib.api.util.VoxelShapeUtil;
+import net.pitan76.mcpitanlib.api.event.block.*;
+import net.pitan76.mcpitanlib.api.util.*;
+import net.pitan76.mcpitanlib.api.util.world.WorldAccessUtil;
 import net.pitan76.mcpitanlib.core.serialization.CompatMapCodec;
 
-public class EMCCable extends EMCRepeater implements IUseableWrench {
+public class EMCCable extends EMCRepeater implements IUseableWrench, Waterloggable {
 
     protected CompatMapCodec<? extends Block> CODEC = CompatMapCodec.createCodecOfExtendBlock(EMCCable::new);
+    public static final BooleanProperty WATERLOGGED = PropertyUtil.waterlogged();
 
     public static final BooleanProperty SIDE1 = PropertyUtil.createBooleanProperty("side1");
     public static final BooleanProperty SIDE2 = PropertyUtil.createBooleanProperty("side2");
@@ -30,8 +30,16 @@ public class EMCCable extends EMCRepeater implements IUseableWrench {
     public static final VoxelShape NS_BOTH_CONNECT = VoxelShapeUtil.blockCuboid(0.0D, 6.0D, 6.0D, 16.0D, 10.0D, 10.0D);
     public static final VoxelShape EW_BOTH_CONNECT = VoxelShapeUtil.blockCuboid(6.0D, 6.0D, 0.0D, 10.0D, 10.0D, 16.0D);
 
-    public static final VoxelShape ONE_CONNECT = VoxelShapeUtil.blockCuboid(0.0D, 6.0D, 6.0D, 13.0D, 10.0D, 10.0D);
-    public static final VoxelShape CONNER_CONNECT = VoxelShapeUtil.union(VoxelShapeUtil.blockCuboid(0.0D, 6.0D, 6.0D, 10.0D, 10.0D, 10.0D), VoxelShapeUtil.blockCuboid(6.0D, 6.0D, 0.0D, 10.0D, 10.0D, 10.0D));
+    public static final VoxelShape NS_ONE_CONNECT_SIDE1 = VoxelShapeUtil.blockCuboid(0.0D, 6.0D, 6.0D, 13.0D, 10.0D, 10.0D);
+    public static final VoxelShape EW_ONE_CONNECT_SIDE1 = VoxelShapeUtil.blockCuboid(6.0D, 6.0D, 0.0D, 10.0D, 10.0D, 13.0D);
+
+    public static final VoxelShape NS_ONE_CONNECT_SIDE2 = VoxelShapeUtil.blockCuboid(3.0D, 6.0D, 6.0D, 16.0D, 10.0D, 10.0D);
+    public static final VoxelShape EW_ONE_CONNECT_SIDE2 = VoxelShapeUtil.blockCuboid(6.0D, 6.0D, 3.0D, 10.0D, 10.0D, 16.0D);
+
+    public static final VoxelShape NORTH_CONNER_CONNECT = VoxelShapeUtil.union(VoxelShapeUtil.blockCuboid(0.0D, 6.0D, 6.0D, 10.0D, 10.0D, 10.0D), VoxelShapeUtil.blockCuboid(6.0D, 6.0D, 0.0D, 10.0D, 10.0D, 10.0D));
+    public static final VoxelShape EAST_CONNER_CONNECT = VoxelShapeUtil.union(VoxelShapeUtil.blockCuboid(6.0D, 6.0D, 6.0D, 16.0D, 10.0D, 10.0D), VoxelShapeUtil.blockCuboid(6.0D, 6.0D, 0.0D, 10.0D, 10.0D, 10.0D));
+    public static final VoxelShape WEST_CONNER_CONNECT = VoxelShapeUtil.union(VoxelShapeUtil.blockCuboid(6.0D, 6.0D, 6.0D, 10.0D, 10.0D, 16.0D), VoxelShapeUtil.blockCuboid(0.0D, 6.0D, 6.0D, 10.0D, 10.0D, 10.0D));
+    public static final VoxelShape SOUTH_CONNER_CONNECT = VoxelShapeUtil.union(VoxelShapeUtil.blockCuboid(6.0D, 6.0D, 6.0D, 10.0D, 10.0D, 16.0D), VoxelShapeUtil.blockCuboid(6.0D, 6.0D, 6.0D, 16.0D, 10.0D, 10.0D));
 
     @Override
     public CompatMapCodec<? extends Block> getCompatCodec() {
@@ -40,40 +48,58 @@ public class EMCCable extends EMCRepeater implements IUseableWrench {
 
     public EMCCable(CompatibleBlockSettings settings) {
         super(settings);
-        setNewDefaultState(getNewDefaultState().with(SIDE1, false).with(SIDE2, false).with(CONNER, false).with(FACING, Direction.NORTH));
+        setNewDefaultState(getNewDefaultState().with(SIDE1, false).with(SIDE2, false).with(CONNER, false).with(FACING, Direction.NORTH).with(WATERLOGGED, false));
     }
 
     public EMCCable() {
         super();
-        setNewDefaultState(getNewDefaultState().with(SIDE1, false).with(SIDE2, false).with(CONNER, false).with(FACING, Direction.NORTH));
+        setNewDefaultState(getNewDefaultState().with(SIDE1, false).with(SIDE2, false).with(CONNER, false).with(FACING, Direction.NORTH).with(WATERLOGGED, false));
     }
 
     @Override
     public void appendProperties(AppendPropertiesArgs args) {
         super.appendProperties(args);
-        args.addProperty(SIDE1, SIDE2, CONNER, FACING);
+        args.addProperty(SIDE1, SIDE2, CONNER, FACING, WATERLOGGED);
     }
 
     @Override
-    public BlockState getStateForNeighborUpdate(BlockState state, Direction direction, BlockState neighborState, WorldAccess world, BlockPos pos, BlockPos neighborPos) {
-        // Cableを繋げる処理、同じ方向にCableがある場合はCONNERをfalseとして繋げる
-        // SIDE1とSIDE2は片方、両方の場合はSIDE1、SIDE2をtrueにする
-        //BlockEntity blockEntity = world.getBlockEntity(neighborPos);
+    public boolean canPathfindThrough(CanPathfindThroughArgs args) {
+        return false;
+    }
 
-        BlockState north = world.getBlockState(pos.north());
-        BlockState south = world.getBlockState(pos.south());
-        BlockState east = world.getBlockState(pos.east());
-        BlockState west = world.getBlockState(pos.west());
-        BlockState up = world.getBlockState(pos.up());
-        BlockState down = world.getBlockState(pos.down());
+    @Override
+    public BlockState getPlacementState(PlacementStateArgs args) {
+        return super.getPlacementState(args).with(WATERLOGGED, WorldUtil.getFluidState(args.getWorld(), args.getPos()).getFluid() == FluidUtil.water());
+    }
 
-        if (neighborState.getBlock() == this || world.getBlockEntity(neighborPos) instanceof EMCStorageBlockEntity) {
-            boolean north_only = north.getBlock() == this || world.getBlockEntity(pos.north()) instanceof EMCStorageBlockEntity;
-            boolean south_only = south.getBlock() == this || world.getBlockEntity(pos.south()) instanceof EMCStorageBlockEntity;
-            boolean east_only = east.getBlock() == this || world.getBlockEntity(pos.east()) instanceof EMCStorageBlockEntity;
-            boolean west_only = west.getBlock() == this || world.getBlockEntity(pos.west()) instanceof EMCStorageBlockEntity;
-            boolean up_only = up.getBlock() == this || world.getBlockEntity(pos.up()) instanceof EMCStorageBlockEntity;
-            boolean down_only = down.getBlock() == this || world.getBlockEntity(pos.down()) instanceof EMCStorageBlockEntity;
+    @Override
+    public FluidState getFluidState(FluidStateArgs args) {
+        return PropertyUtil.get(args.state, WATERLOGGED) ? FluidUtil.getStillWater() : super.getFluidState(args);
+    }
+
+    @Override
+    public BlockState getStateForNeighborUpdate(StateForNeighborUpdateArgs args) {
+        BlockState state = args.getState();
+        BlockPos pos = args.getPos();
+
+        if (state.get(WATERLOGGED))
+            WorldAccessUtil.scheduleFluidTick(args.world, pos, FluidUtil.water(), FluidUtil.getTickRate(FluidUtil.water(), args.world));
+
+        BlockState north = args.getBlockState(pos.north());
+        BlockState south = args.getBlockState(pos.south());
+        BlockState east = args.getBlockState(pos.east());
+        BlockState west = args.getBlockState(pos.west());
+        BlockState up = args.getBlockState(pos.up());
+        BlockState down = args.getBlockState(pos.down());
+
+        boolean north_only = north.getBlock() == this || args.getBlockEntity(pos.north()) instanceof EMCStorageBlockEntity;
+        boolean south_only = south.getBlock() == this || args.getBlockEntity(pos.south()) instanceof EMCStorageBlockEntity;
+        boolean east_only = east.getBlock() == this || args.getBlockEntity(pos.east()) instanceof EMCStorageBlockEntity;
+        boolean west_only = west.getBlock() == this || args.getBlockEntity(pos.west()) instanceof EMCStorageBlockEntity;
+        boolean up_only = up.getBlock() == this || args.getBlockEntity(pos.up()) instanceof EMCStorageBlockEntity;
+        boolean down_only = down.getBlock() == this || args.getBlockEntity(pos.down()) instanceof EMCStorageBlockEntity;
+
+        if (north_only || south_only || east_only || west_only || up_only || down_only) {
 
             boolean both_ns = (north_only && south_only);
             boolean both_ew = (east_only && west_only);
@@ -90,7 +116,17 @@ public class EMCCable extends EMCRepeater implements IUseableWrench {
             if (both_ns || both_ew || both_ud) {
                 return state.with(CONNER, false).with(SIDE1, true).with(SIDE2, true);
             } else if (north_only && east_only || north_only && west_only || south_only && east_only || south_only && west_only) {
-                return state.with(CONNER, true).with(SIDE1, false).with(SIDE2, false);
+                if (north_only && west_only ) {
+                    return state.with(CONNER, true).with(SIDE1, false).with(SIDE2, false).with(FACING, Direction.NORTH);
+                } else if (north_only && east_only) {
+                    return state.with(CONNER, true).with(SIDE1, false).with(SIDE2, false).with(FACING, Direction.EAST);
+                } else if (south_only && west_only) {
+                    return state.with(CONNER, true).with(SIDE1, false).with(SIDE2, false).with(FACING, Direction.WEST);
+                } else if (south_only && east_only) {
+                    return state.with(CONNER, true).with(SIDE1, false).with(SIDE2, false).with(FACING, Direction.SOUTH);
+                }
+
+
             } else if (north_only || west_only) {
                 return state.with(CONNER, false).with(SIDE1, true).with(SIDE2, false);
             } else if (south_only || east_only) {
@@ -98,22 +134,42 @@ public class EMCCable extends EMCRepeater implements IUseableWrench {
             }
         }
 
-        return state.with(CONNER, false).with(SIDE1, false).with(SIDE2, false);
+        return state.with(CONNER, false).with(SIDE1, false).with(SIDE2, false).with(FACING, Direction.NORTH);
 
     }
 
     @Override
     public VoxelShape getOutlineShape(OutlineShapeEvent e) {
+        Direction direction = e.getState().get(FACING);
+
         if (e.getState().get(SIDE1) && e.getState().get(SIDE2)) {
-            if (e.getState().get(FACING) == Direction.NORTH || e.getState().get(FACING) == Direction.SOUTH) {
+            if (direction == Direction.NORTH) {
                 return NS_BOTH_CONNECT;
             } else {
                 return EW_BOTH_CONNECT;
             }
-        } else if (e.getState().get(SIDE1) || e.getState().get(SIDE2)) {
-            return ONE_CONNECT;
+        } else if (e.getState().get(SIDE1)) {
+            if (direction == Direction.NORTH) {
+                return NS_ONE_CONNECT_SIDE1;
+            } else {
+                return EW_ONE_CONNECT_SIDE1;
+            }
+        } else if (e.getState().get(SIDE2)) {
+            if (direction == Direction.NORTH) {
+                return NS_ONE_CONNECT_SIDE2;
+            } else {
+                return EW_ONE_CONNECT_SIDE2;
+            }
         } else if (e.getState().get(CONNER)) {
-            return CONNER_CONNECT;
+            if (direction == Direction.NORTH) {
+                return NORTH_CONNER_CONNECT;
+            } else if (direction == Direction.SOUTH) {
+                return SOUTH_CONNER_CONNECT;
+            } else if (direction == Direction.EAST) {
+                return EAST_CONNER_CONNECT;
+            } else if (direction == Direction.WEST) {
+                return WEST_CONNER_CONNECT;
+            }
         }
         return NONE;
     }
