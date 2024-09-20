9ebe530b03a2b89bc0e2178b1096107b0443a624
PTOM76 <58260965+PTOM76@users.noreply.github.com>
2024-09-20 16:30:17 +0900

    #122

--- src/main/java/net/pitan76/itemalchemy/block/AlchemyTable.java
+++ src/main/java/net/pitan76/itemalchemy/block/AlchemyTable.java
@@ -1,23 +1,32 @@
 package net.pitan76.itemalchemy.block;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.MapColor;
+import net.minecraft.state.property.DirectionProperty;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.math.Direction;
 import net.minecraft.util.shape.VoxelShape;
 import net.pitan76.itemalchemy.EMCManager;
 import net.pitan76.itemalchemy.gui.AlchemyTableScreenHandlerFactory;
 import net.pitan76.mcpitanlib.api.block.CompatibleBlockSettings;
 import net.pitan76.mcpitanlib.api.block.ExtendBlock;
 import net.pitan76.mcpitanlib.api.entity.Player;
+import net.pitan76.mcpitanlib.api.event.block.AppendPropertiesArgs;
 import net.pitan76.mcpitanlib.api.event.block.BlockUseEvent;
 import net.pitan76.mcpitanlib.api.event.block.OutlineShapeEvent;
+import net.pitan76.mcpitanlib.api.event.block.PlacementStateArgs;
+import net.pitan76.mcpitanlib.api.util.PropertyUtil;
 import net.pitan76.mcpitanlib.api.util.VoxelShapeUtil;
 import net.pitan76.mcpitanlib.core.serialization.CompatMapCodec;
+import org.jetbrains.annotations.Nullable;
 
 public class AlchemyTable extends ExtendBlock implements IUseableWrench {
 
     protected CompatMapCodec<? extends Block> CODEC = CompatMapCodec.createCodecOfExtendBlock(AlchemyTable::new);
+
+    public static final DirectionProperty FACING = PropertyUtil.facing();
 
     @Override
     public CompatMapCodec<? extends Block> getCompatCodec() {
@@ -26,6 +35,7 @@ public class AlchemyTable extends ExtendBlock implements IUseableWrench {
 
     public AlchemyTable(CompatibleBlockSettings settings) {
         super(settings);
+        setNewDefaultState(getNewDefaultState().with(FACING, Direction.DOWN));
     }
 
     public AlchemyTable() {
@@ -46,6 +56,33 @@ public class AlchemyTable extends ExtendBlock implements IUseableWrench {
 
     @Override
     public VoxelShape getOutlineShape(OutlineShapeEvent e) {
-        return VoxelShapeUtil.cuboid(0, 0, 0, 1, 0.125, 1);
+        Direction dir = e.state.contains(FACING) ? e.getProperty(FACING) : Direction.DOWN;
+
+        switch (dir) {
+            case UP:
+                return VoxelShapeUtil.cuboid(0, 0.875, 0, 1, 1, 1);
+            case NORTH:
+                return VoxelShapeUtil.cuboid(0, 0, 0, 1, 1, 0.125);
+            case SOUTH:
+                return VoxelShapeUtil.cuboid(0, 0, 0.875, 1, 1, 1);
+            case WEST:
+                return VoxelShapeUtil.cuboid(0, 0, 0, 0.125, 1, 1);
+            case EAST:
+                return VoxelShapeUtil.cuboid(0.875, 0, 0, 1, 1, 1);
+            default: // DOWN
+                return VoxelShapeUtil.cuboid(0, 0, 0, 1, 0.125, 1);
+        }
+    }
+
+    @Override
+    public void appendProperties(AppendPropertiesArgs args) {
+        super.appendProperties(args);
+        args.addProperty(FACING);
+    }
+
+    @Override
+    public BlockState getPlacementState(PlacementStateArgs args) {
+
+        return args.withBlockState(FACING, args.getSide().getOpposite());
     }
 }
