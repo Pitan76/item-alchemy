8044260889d2a6691797309e01ac0f032c77742a
PTOM76 <58260965+PTOM76@users.noreply.github.com>
2024-10-26 14:40:42 +0900

    recipeRemainder

--- src/main/java/net/pitan76/itemalchemy/item/Items.java
+++ src/main/java/net/pitan76/itemalchemy/item/Items.java
@@ -70,7 +70,7 @@ public class Items {
     public static RegistryResult<Item> RED_MATTER_HOE;
 
     public static void init() {
-        PHILOSOPHER_STONE = registry.registerItem(_id("philosopher_stone"), () -> new PhilosopherStone(ChargeItemSettings.of().addGroup(ItemGroups.ITEM_ALCHEMY).rarity(Rarity.EPIC)));
+        PHILOSOPHER_STONE = registry.registerItem(_id("philosopher_stone"), () -> new PhilosopherStone(ChargeItemSettings.of().addGroup(ItemGroups.ITEM_ALCHEMY).rarity(Rarity.EPIC).recipeRemainder(Items.PHILOSOPHER_STONE.getOrNull())));
         ALCHEMY_TABLE = registry.registerItem(_id("alchemy_table"), () -> ItemUtil.ofBlock(Blocks.ALCHEMY_TABLE.getOrNull(), CompatibleItemSettings.of().addGroup(ItemGroups.ITEM_ALCHEMY)));
         TOME_OF_KNOWLEDGE = registry.registerItem(_id("tome_of_knowledge"), () -> new TomeOfKnowledge(CompatibleItemSettings.of().rarity(Rarity.EPIC).maxCount(1).addGroup(ItemGroups.ITEM_ALCHEMY)));
         EMC_COLLECTOR_MK1 = registry.registerItem(_id("emc_collector_mk1"), () -> ItemUtil.ofBlock(Blocks.EMC_COLLECTOR_MK1.getOrNull(), CompatibleItemSettings.of().addGroup(ItemGroups.ITEM_ALCHEMY)));
