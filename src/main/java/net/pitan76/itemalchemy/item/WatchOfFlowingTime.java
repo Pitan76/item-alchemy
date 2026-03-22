256f03ede9f216b36312aece194001e0c85d0c92
PTOM76 <ptms76@outlook.jp>
2026-03-23 03:07:12 +0900

    [cp] refactor: optimize WatchOfFlowingTime with utility methods and improved block entity handling

--- src/main/java/net/pitan76/itemalchemy/item/WatchOfFlowingTime.java
+++ src/main/java/net/pitan76/itemalchemy/item/WatchOfFlowingTime.java
@@ -1,5 +1,6 @@
 package net.pitan76.itemalchemy.item;
 
+import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.server.world.ServerWorld;
@@ -12,6 +13,11 @@ import net.pitan76.itemalchemy.tile.DMPedestalTile;
 import net.pitan76.mcpitanlib.api.item.v2.CompatItem;
 import net.pitan76.mcpitanlib.api.item.v2.CompatibleItemSettings;
 import net.pitan76.mcpitanlib.api.util.BlockStateUtil;
+import net.pitan76.mcpitanlib.api.util.WorldUtil;
+import net.pitan76.mcpitanlib.api.util.math.BoxUtil;
+import net.pitan76.mcpitanlib.api.util.math.PosUtil;
+import net.pitan76.mcpitanlib.api.util.world.TickerUtil;
+import net.pitan76.mcpitanlib.midohra.block.entity.BlockEntityWrapper;
 
 import java.util.List;
 
@@ -28,9 +34,11 @@ public class WatchOfFlowingTime extends CompatItem implements IPedestalItem {
     public boolean updateInPedestal(ItemStack stack, World world, BlockPos pos) {
         if (world.isClient()) return false;
 
-        Box effectBox = new Box(
-                pos.getX() - DMPedestalTile.RANGE, pos.getY() - DMPedestalTile.RANGE, pos.getZ() - DMPedestalTile.RANGE,
-                pos.getX() + DMPedestalTile.RANGE + 1, pos.getY() + DMPedestalTile.RANGE + 1, pos.getZ() + DMPedestalTile.RANGE + 1
+        net.pitan76.mcpitanlib.midohra.util.math.BlockPos pos2 = net.pitan76.mcpitanlib.midohra.util.math.BlockPos.of(pos);
+
+        Box effectBox = BoxUtil.createBox(
+                pos2.getX() - DMPedestalTile.RANGE, pos2.getY() - DMPedestalTile.RANGE, pos2.getZ() - DMPedestalTile.RANGE,
+                pos2.getX() + DMPedestalTile.RANGE + 1, pos2.getY() + DMPedestalTile.RANGE + 1, pos2.getZ() + DMPedestalTile.RANGE + 1
         );
 
         speedUpBlockEntities(world, effectBox);
@@ -42,21 +50,22 @@ public class WatchOfFlowingTime extends CompatItem implements IPedestalItem {
 
     private void speedUpBlockEntities(World world, Box box) {
         if (!(world instanceof ServerWorld)) return;
-        ServerWorld serverWorld = (ServerWorld) world;
+//        ServerWorld serverWorld = (ServerWorld) world;
 
-        BlockPos min = new BlockPos((int) box.minX, (int) box.minY, (int) box.minZ);
-        BlockPos max = new BlockPos((int) box.maxX, (int) box.maxY, (int) box.maxZ);
+        BlockPos min = PosUtil.flooredBlockPos((int) box.minX, (int) box.minY, (int) box.minZ);
+        BlockPos max = PosUtil.flooredBlockPos((int) box.maxX, (int) box.maxY, (int) box.maxZ);
 
-        for (BlockPos pos : BlockPos.iterate(min, max)) {
-            BlockEntity be = world.getBlockEntity(pos);
-            if (be != null && !be.isRemoved() && !(be instanceof DMPedestalTile)) {
-                net.minecraft.block.BlockState state = world.getBlockState(pos);
-                net.minecraft.block.entity.BlockEntityTicker<?> ticker = state.getBlockEntityTicker(world, be.getType());
-                if (ticker != null) {
-                    for (int i = 0; i < BONUS_TICKS; i++) {
-                        ((net.minecraft.block.entity.BlockEntityTicker) ticker).tick(world, pos, state, be);
-                    }
+        for (BlockPos pos : PosUtil.iterate(min, max)) {
+            BlockEntity be = WorldUtil.getBlockEntity(world, pos);
+            BlockEntityWrapper beWrapper = BlockEntityWrapper.of(be);
+            if (be != null && !beWrapper.isRemoved() && !(be instanceof DMPedestalTile)) {
+                BlockState state = WorldUtil.getBlockState(world, pos);
+//                net.minecraft.block.entity.BlockEntityTicker<?> ticker = state.getBlockEntityTicker(world, be.getType());
+//                if (ticker != null) {
+                for (int i = 0; i < BONUS_TICKS; i++) {
+                    TickerUtil.tick(be, world, pos, state);
                 }
+//                }
             }
         }
     }
